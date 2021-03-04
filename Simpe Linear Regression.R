d87022ffa2ee23f76e0bc66f7d8f5158e2a53ed9
Heli-helicopter <70734528+Heli-helicopter@users.noreply.github.com>
2021-03-05 10:16:57 +1100

    do it

--- Simpe Linear Regression.R
+++ Simpe Linear Regression.R
@@ -2,7 +2,6 @@ library(MASS)
 
 names(Boston)
 dim(Boston)
-fix(Boston)
 summary(Boston)
 
 attach(Boston)
@@ -20,8 +19,42 @@ detach(Boston)
 
 lm.fit <- lm(medv~lstat, data=Boston)
 summary(lm.fit)
+confint(lm.fit)
 
 attach(Boston)
-plot(lstat,medv, xlab = 'lstat', ylab = 'medv', main = 'Scatter pot of medv vs lstat with east Squars regression line')
+png("Scatter with regression line.png")
+plot(lstat,medv, xlab = '% Lower status population', ylab = 'Median home vaue in $1000', main = 'Scatter plot of median home vaue vs % lower status population with regression line')
 abline(lm.fit,col='red', lwd=2)
+dev.off()
 detach(Boston)
+
+
+par(mfrow=c(2,2))
+plot(lm.fit) #diagnostic plots
+
+
+#suggest the model may be improved by removing outliers.
+
+#outliers removed based on interquantie range.
+
+attach(Boston)
+outliers <- boxplot(medv, plot=FALSE)$out
+print(outliers)
+df_removed_outliers <- Boston[-which(Boston$medv %in% outliers),]
+
+lm.outliers <- lm(medv~lstat, data=df_removed_outliers)
+summary(lm.outliers)
+confint(lm.outliers)
+
+par(mfrow=c(2,2))
+plot(lm.outliers)
+
+
+png("Scatter with regression line (outliers removed).png")
+plot(lstat,medv, xlab = '% Lower status population', ylab = 'Median home vaue in $1000', main = 'Scatter plot of median home vaue vs % lower status population with regression line (outliers removed)')
+abline(lm.outliers,col='red', lwd=2)
+dev.off()
+detach(Boston)
+
+
+
